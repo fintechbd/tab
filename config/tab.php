951ecc1ccf769ff86c357c6b9ea88903c93954c6
Mohammad Hafijul Islam <hafijul233@gmail.com>
2024-01-06 00:52:15 +0600

    Tab Key Added in ENV

--- config/tab.php
+++ config/tab.php
@@ -9,7 +9,7 @@ return [
     |--------------------------------------------------------------------------
     | This setting enable the API will be available or not
     */
-    'enabled' => env('PACKAGE_Tab_ENABLED', true),
+    'enabled' => env('PACKAGE_TAB_ENABLED', true),
 
     /*
     |--------------------------------------------------------------------------
