2aa8585a3bd44f47421cd2ba3ff4b9ce37dcc7c4
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-01-06 02:58:59 +0600

    LP-199 trait add

---
+++ src/Traits/AuthRelations.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Fintech\Tab\Traits;
+
+use Fintech\Core\Facades\Core;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+if (Core::packageExists('Auth')) {
+    trait AuthRelations
+    {
+        public function senderReceiver(): BelongsTo
+        {
+            return $this->belongsTo(
+                config('fintech.auth.user_model', \Fintech\Auth\Models\User::class),
+                'sender_receiver_id');
+        }
+
+        public function user(): BelongsTo
+        {
+            return $this->belongsTo(
+                config('fintech.auth.user_model', \Fintech\Auth\Models\User::class),
+                'user_id');
+        }
+    }
+} else {
+    trait AuthRelations
+    {
+    }
+}
