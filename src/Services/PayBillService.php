02bda0a6ae8ba2ccd83653f4cbf6fc15efcc0f81
MD ARIFUL HAQUE <mah.shamim@gmail.com>
2024-02-02 03:18:15 +0600

    LP-199 pay bill service and repository done

---
+++ src/Services/PayBillService.php
@@ -0,0 +1,267 @@
+<?php
+
+namespace Fintech\Tab\Services;
+
+
+use Fintech\Tab\Interfaces\PayBillRepository;
+use Fintech\Transaction\Facades\Transaction;
+
+/**
+ * Class PayBillService
+ * @package Fintech\Tab\Services
+ *
+ */
+class PayBillService
+{
+    /**
+     * PayBillService constructor.
+     * @param PayBillRepository $payBillRepository
+     */
+    public function __construct(PayBillRepository $payBillRepository) {
+        $this->payBillRepository = $payBillRepository;
+    }
+
+    /**
+     * @param array $filters
+     * @return mixed
+     */
+    public function list(array $filters = [])
+    {
+        return $this->payBillRepository->list($filters);
+
+    }
+
+    public function create(array $inputs = [])
+    {
+        return $this->payBillRepository->create($inputs);
+    }
+
+    public function find($id, $onlyTrashed = false)
+    {
+        return $this->payBillRepository->find($id, $onlyTrashed);
+    }
+
+    public function update($id, array $inputs = [])
+    {
+        return $this->payBillRepository->update($id, $inputs);
+    }
+
+    public function destroy($id)
+    {
+        return $this->payBillRepository->delete($id);
+    }
+
+    public function restore($id)
+    {
+        return $this->payBillRepository->restore($id);
+    }
+
+    public function export(array $filters)
+    {
+        return $this->payBillRepository->list($filters);
+    }
+
+    public function import(array $filters)
+    {
+        return $this->payBillRepository->create($filters);
+    }
+    /**
+     * @return int[]
+     */
+    public function debitTransaction($data): array
+    {
+        $userAccountData = [
+            'previous_amount' => null,
+            'current_amount' => null,
+            'spent_amount' => null,
+        ];
+
+        //Collect Current Balance as Previous Balance
+        $userAccountData['previous_amount'] = Transaction::orderDetail()->list([
+            'get_order_detail_amount_sum' => true,
+            'user_id' => $data->user_id,
+            'order_detail_currency' => $data->converted_currency,
+        ]);
+
+        $serviceStatData = $data->order_data['service_stat_data'];
+        $master_user_name = $data->order_data['master_user_name'];
+        $user_name = $data->order_data['user_name'];
+
+        $amount = $data->amount;
+        $converted_amount = $data->converted_amount;
+        $data->amount = -$amount;
+        $data->converted_amount = -$converted_amount;
+        $data->order_detail_cause_name = 'cash_withdraw';
+        $data->order_detail_number = $data->order_data['purchase_number'];
+        $data->order_detail_response_id = $data->order_data['purchase_number'];
+        $data->notes = 'Pay Bill Payment Send to '.$master_user_name;
+        $orderDetailStore = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStore->order_detail_parent_id = $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStore->save();
+        $orderDetailStore->fresh();
+        $orderDetailStoreForMaster = $orderDetailStore->replicate();
+        $orderDetailStoreForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForMaster->order_detail_amount = $amount;
+        $orderDetailStoreForMaster->converted_amount = $converted_amount;
+        $orderDetailStoreForMaster->step = 2;
+        $orderDetailStoreForMaster->notes = 'Pay Bill Payment Receive From'.$user_name;
+        $orderDetailStoreForMaster->save();
+
+        //For Charge
+        $data->amount = calculate_flat_percent($amount, $serviceStatData['charge']);
+        $data->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['charge']);
+        $data->order_detail_cause_name = 'charge';
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $data->notes = 'Pay Bill Charge Send to '.$master_user_name;
+        $data->step = 3;
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStoreForCharge = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStoreForChargeForMaster = $orderDetailStoreForCharge->replicate();
+        $orderDetailStoreForChargeForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForChargeForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForChargeForMaster->order_detail_amount = -calculate_flat_percent($amount, $serviceStatData['charge']);
+        $orderDetailStoreForChargeForMaster->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['charge']);
+        $orderDetailStoreForChargeForMaster->order_detail_cause_name = 'charge';
+        $orderDetailStoreForChargeForMaster->notes = 'Pay Bill Charge Receive from '.$user_name;
+        $orderDetailStoreForChargeForMaster->step = 4;
+        $orderDetailStoreForChargeForMaster->save();
+
+        //For Discount
+        $data->amount = -calculate_flat_percent($amount, $serviceStatData['discount']);
+        $data->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['discount']);
+        $data->order_detail_cause_name = 'discount';
+        $data->notes = 'Pay Bill Discount form '.$master_user_name;
+        $data->step = 5;
+        //$data->order_detail_parent_id = $orderDetailStore->getKey();
+        //$updateData['order_data']['previous_amount'] = 0;
+        $orderDetailStoreForDiscount = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStoreForDiscountForMaster = $orderDetailStoreForCharge->replicate();
+        $orderDetailStoreForDiscountForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForDiscountForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForDiscountForMaster->order_detail_amount = calculate_flat_percent($amount, $serviceStatData['discount']);
+        $orderDetailStoreForDiscountForMaster->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['discount']);
+        $orderDetailStoreForDiscountForMaster->order_detail_cause_name = 'discount';
+        $orderDetailStoreForDiscountForMaster->notes = 'Pay Bill Discount to '.$user_name;
+        $orderDetailStoreForDiscountForMaster->step = 6;
+        $orderDetailStoreForDiscountForMaster->save();
+
+        //'Point Transfer Commission Send to ' . $masterUser->name;
+        //'Point Transfer Commission Receive from ' . $receiver->name;
+
+        $userAccountData['current_amount'] = Transaction::orderDetail()->list([
+            'get_order_detail_amount_sum' => true,
+            'user_id' => $data->user_id,
+            'converted_currency' => $data->converted_currency,
+        ]);
+
+        $userAccountData['spent_amount'] = Transaction::orderDetail()->list([
+            'get_order_detail_amount_sum' => true,
+            'user_id' => $data->user_id,
+            'order_id' => $data->getKey(),
+            'converted_currency' => $data->converted_currency,
+        ]);
+
+        return $userAccountData;
+
+    }
+
+    /**
+     * @return int[]
+     */
+    public function creditTransaction($data): array
+    {
+        $userAccountData = [
+            'previous_amount' => null,
+            'current_amount' => null,
+            'spent_amount' => null,
+        ];
+
+        //Collect Current Balance as Previous Balance
+        $userAccountData['previous_amount'] = Transaction::orderDetail()->list([
+            'get_order_detail_amount_sum' => true,
+            'user_id' => $data->user_id,
+            'converted_currency' => $data->converted_currency,
+        ]);
+
+        $serviceStatData = $data->order_data['service_stat_data'];
+        $master_user_name = $data->order_data['master_user_name'];
+        $user_name = $data->order_data['user_name'];
+
+        $data->order_detail_cause_name = 'cash_withdraw';
+        $data->order_detail_number = $data->order_data['accepted_number'];
+        $data->order_detail_response_id = $data->order_data['purchase_number'];
+        $data->notes = 'Pay Bill Refund From '.$master_user_name;
+        $orderDetailStore = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStore->order_detail_parent_id = $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStore->save();
+        $orderDetailStore->fresh();
+        $amount = $data->amount;
+        $converted_amount = $data->converted_amount;
+        $orderDetailStoreForMaster = $orderDetailStore->replicate();
+        $orderDetailStoreForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForMaster->order_detail_amount = -$amount;
+        $orderDetailStoreForMaster->converted_amount = -$converted_amount;
+        $orderDetailStoreForMaster->step = 2;
+        $orderDetailStoreForMaster->notes = 'Pay Bill Send to '.$user_name;
+        $orderDetailStoreForMaster->save();
+
+        //For Charge
+        $data->amount = -calculate_flat_percent($amount, $serviceStatData['charge']);
+        $data->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['charge']);
+        $data->order_detail_cause_name = 'charge';
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $data->notes = 'Pay Bill Charge Receive from '.$master_user_name;
+        $data->step = 3;
+        $data->order_detail_parent_id = $orderDetailStore->getKey();
+        $orderDetailStoreForCharge = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStoreForChargeForMaster = $orderDetailStoreForCharge->replicate();
+        $orderDetailStoreForChargeForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForChargeForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForChargeForMaster->order_detail_amount = calculate_flat_percent($amount, $serviceStatData['charge']);
+        $orderDetailStoreForChargeForMaster->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['charge']);
+        $orderDetailStoreForChargeForMaster->order_detail_cause_name = 'charge';
+        $orderDetailStoreForChargeForMaster->notes = 'Pay Bill Charge Send to '.$user_name;
+        $orderDetailStoreForChargeForMaster->step = 4;
+        $orderDetailStoreForChargeForMaster->save();
+
+        //For Discount
+        $data->amount = calculate_flat_percent($amount, $serviceStatData['discount']);
+        $data->converted_amount = calculate_flat_percent($converted_amount, $serviceStatData['discount']);
+        $data->order_detail_cause_name = 'discount';
+        $data->notes = 'Pay Bill Discount form '.$master_user_name;
+        $data->step = 5;
+        //$data->order_detail_parent_id = $orderDetailStore->getKey();
+        $updateData['order_data']['previous_amount'] = 0;
+        $orderDetailStoreForDiscount = Transaction::orderDetail()->create(Transaction::orderDetail()->orderDetailsDataArrange($data));
+        $orderDetailStoreForDiscountForMaster = $orderDetailStoreForCharge->replicate();
+        $orderDetailStoreForDiscountForMaster->user_id = $data->sender_receiver_id;
+        $orderDetailStoreForDiscountForMaster->sender_receiver_id = $data->user_id;
+        $orderDetailStoreForDiscountForMaster->order_detail_amount = -calculate_flat_percent($amount, $serviceStatData['discount']);
+        $orderDetailStoreForDiscountForMaster->converted_amount = -calculate_flat_percent($converted_amount, $serviceStatData['discount']);
+        $orderDetailStoreForDiscountForMaster->order_detail_cause_name = 'discount';
+        $orderDetailStoreForDiscountForMaster->notes = 'Pay Bill Discount to '.$user_name;
+        $orderDetailStoreForDiscountForMaster->step = 6;
+        $orderDetailStoreForDiscountForMaster->save();
+
+        //'Point Transfer Commission Send to ' . $masterUser->name;
+        //'Point Transfer Commission Receive from ' . $receiver->name;
+
+        $userAccountData['current_amount'] = Transaction::orderDetail()->list([
+            'get_order_detail_amount_sum' => true,
+            'user_id' => $data->user_id,
+            'converted_currency' => $data->converted_currency,
+        ]);
+
+        $userAccountData['spent_amount'] = Transaction::orderDetail()->list([
+            'get_order_detail_amount_sum' => true,
+            'user_id' => $data->user_id,
+            'order_id' => $data->getKey(),
+            'converted_currency' => $data->converted_currency,
+        ]);
+
+        return $userAccountData;
+
+    }
+}
