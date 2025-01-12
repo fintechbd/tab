9790cb31350349d3a465955731fa5a2796f6b672
Mohammad Hafijul Islam <hafijul233@gmail.com>
2025-01-12 18:21:46 +0600

    translation cleaned

--- lang/en/messages.php
+++ lang/en/messages.php
@@ -12,14 +12,4 @@
 */
 
 return [
-    'assign_vendor' => [
-        'not_found' => ':slug vendor is not available on the configurations.',
-        'not_assigned' => 'This order have not been assigned to any vendor.',
-        'already_assigned' => 'This order has already assigned by another user.',
-        'assigned_user_failed' => 'Unable to assign this order to requested user.',
-        'failed' => 'Unable to assign order to requested vendor [:slug].',
-        'release_failed' => 'There\'s been an error. while :model with ID::id failed to release.',
-        'success' => 'Successfully assigned order to requested vendor [:slug].',
-        'quote_failed' => 'Something went wrong. Please try again later.',
-    ],
 ];
